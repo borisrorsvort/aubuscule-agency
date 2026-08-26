d455bf8fb7805522d0851e464099c45d5799f3dd
Boris Rorsvort <boris@rorsvort.com>
2026-08-27 00:48:38 +0200

    Add Google Ads script to head

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -60,6 +60,11 @@ export default async function LocaleLayout({
     >
       <head>
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9329042372767031"
+          crossOrigin="anonymous"
+        ></script>
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(){try{var t=localStorage.getItem('theme')||(window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light');document.documentElement.setAttribute('data-theme',t)}catch(e){}})()`,
           }}
